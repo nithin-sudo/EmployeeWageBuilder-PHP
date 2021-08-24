affda24d49334af74ec97dc0d43d6894365c47be
nithin-sudo <nithinkrishnasathram@gmail.com>
2021-08-24 23:41:41 +0530

    Added: Function to check the Employee Attendance.

--- EmployeeWage.php
+++ EmployeeWage.php
@@ -1,3 +1,20 @@
 <?php
-echo "Welcome to Employee wage builder"
+class EmployeeWage
+{
+   /**
+    *Declaring a static function and checking the Employee
+    *Attendance by using rand() function
+    */
+   public static function calcEmployeeWage()
+   {
+    $IS_FULL_TIME = 1;
+    $random = rand(0,1);
+    if($random == $IS_FULL_TIME)
+        print "Employee is present";
+    else
+        print "Employee is absent" ;   
+   }
+}
+#calling the static function by using the class.
+EmployeeWage::calcEmployeeWage();
 ?>
