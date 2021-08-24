60f1ceb02eff9dce52c98a79e3ec7e6c47bf03a6
nithin-sudo <nithinkrishnasathram@gmail.com>
2021-08-24 23:50:24 +0530

    MODIFIED: Method and calculating dailywage of an employee.

--- EmployeeWage.php
+++ EmployeeWage.php
@@ -8,11 +8,22 @@ class EmployeeWage
    public static function calcEmployeeWage()
    {
     $IS_FULL_TIME = 1;
+    $WAGE_PER_HOUR = 20;
+    $FULL_WORK_HOUR = 8;
+
     $random = rand(0,1);
+    
+    //calculating employee daily wage.
     if($random == $IS_FULL_TIME)
+    {
         print "Employee is present";
+        $dailyWage = $WAGE_PER_HOUR * $FULL_WORK_HOUR;
+        print "Daily Wage: ".$dailyWage;
+    }    
     else
+    {
         print "Employee is absent" ;   
+    }    
    }
 }
 #calling the static function by using the class.
