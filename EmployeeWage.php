a1353d95ad1afca91ac7bc4a3aa39d11c44dd32d
nithin-sudo <nithinkrishnasathram@gmail.com>
2021-08-24 23:58:18 +0530

    MODIFIED: Method and adding part time wage of an employee.

--- EmployeeWage.php
+++ EmployeeWage.php
@@ -8,9 +8,10 @@ class EmployeeWage
    public static function calcEmployeeWage()
    {
     $IS_FULL_TIME = 1;
+    $IS_PART_TIME = 0;
     $WAGE_PER_HOUR = 20;
     $FULL_WORK_HOUR = 8;
-
+    $PART_WORK_HOUR = 4;
     $random = rand(0,1);
     
     //calculating employee daily wage.
@@ -20,6 +21,12 @@ class EmployeeWage
         $dailyWage = $WAGE_PER_HOUR * $FULL_WORK_HOUR;
         print "Daily Wage: ".$dailyWage;
     }    
+    else if($random == $IS_PART_TIME)
+    {
+        print "Employee is Part time present";
+        $dailyWage = $WAGE_PER_HOUR * $PART_WORK_HOUR;
+        print "Daily Wage: ".$dailyWage;
+    }
     else
     {
         print "Employee is absent" ;   
